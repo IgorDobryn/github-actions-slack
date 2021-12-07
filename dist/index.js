e1d17a4c2002e84cb1d4e7e0f09f61c464dc1548
Igor Dobryn <igor.dobryn@railsware.com>
2021-12-07 14:26:45 +0200

    Yarn build

--- dist/index.js
+++ dist/index.js
@@ -724,40 +724,40 @@ const postMessage = async () => {
   context.info("info: Before try");
   context.debugExtra("debugExtra: Before try");
   context.warning("warning: Before try");
-  try {
-    const token = context.getRequired("slack-bot-user-oauth-access-token");
-    const channels = context.getRequired("slack-channel");
-    const text = context.getRequired("slack-text");
+  // try {
+  const token = context.getRequired("slack-bot-user-oauth-access-token");
+  const channels = context.getRequired("slack-channel");
+  const text = context.getRequired("slack-text");
 
-    context.debug("Prepare");
+  context.debug("Prepare");
 
-    const results = [];
-    for (let channel of channels.split(",")) {
-      channel = channel.trim();
+  const results = [];
+  for (let channel of channels.split(",")) {
+    channel = channel.trim();
 
-      context.debug("Building the message");
-      context.debug("Optional", optional());
-      console.log("Building the message");
-      console.debugExtra("Optional", optional());
-      console.info("Optional", optional());
-      const payload = buildMessage(channel, text, optional());
+    context.debug("Building the message");
+    context.debug("Optional", optional());
+    console.log("Building the message");
+    console.debugExtra("Optional", optional());
+    console.info("Optional", optional());
+    const payload = buildMessage(channel, text, optional());
 
-      context.debug("Post Message PAYLOAD", payload);
-      const result = await apiPostMessage(token, payload);
-      context.debug("Post Message RESULT", result);
+    context.debug("Post Message PAYLOAD", payload);
+    const result = await apiPostMessage(token, payload);
+    context.debug("Post Message RESULT", result);
 
-      results.push(result);
-    }
-
-    // To not break backward compatibility
-    const resultAsJson = jsonPretty(results[0]);
-    context.setOutput("slack-result", resultAsJson);
-
-    const resultsAsJson = jsonPretty(results);
-    context.setOutput("slack-results", resultsAsJson);
-  } catch (error) {
-    context.setFailed(jsonPretty(error));
+    results.push(result);
   }
+
+  // To not break backward compatibility
+  const resultAsJson = jsonPretty(results[0]);
+  context.setOutput("slack-result", resultAsJson);
+
+  const resultsAsJson = jsonPretty(results);
+  context.setOutput("slack-results", resultsAsJson);
+  // } catch (error) {
+  //   context.setFailed(jsonPretty(error));
+  // }
 };
 
 const tryParseJSON = (string) => {
